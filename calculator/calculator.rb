de70f2c937d7353c9296789f4283daba6a49cc8a
Tom Lee <tom_t_lee@yahoo.com>
2015-02-07 20:45:35 +0000

    Cleanup comments in Calculator

--- calculator/calculator.rb
+++ calculator/calculator.rb
@@ -1,9 +1,3 @@
-# take first and second user numbers
-# take user calculation type - plus/minus etc
-# perform the calculation
-# output the answer
-# repeat
-
 def say(message)
   puts "=> #{message}"
 end
