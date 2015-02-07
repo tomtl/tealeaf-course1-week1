14aa7e254094b70901ef0e73f806e6957e9f66a0
Tom Lee <tom_t_lee@yahoo.com>
2015-02-07 20:43:42 +0000

    Calculator

--- calculator/calculator.rb
+++ calculator/calculator.rb
@@ -3,3 +3,36 @@
 # perform the calculation
 # output the answer
 # repeat
+
+def say(message)
+  puts "=> #{message}"
+end
+
+begin
+  say "What is your first number?"
+  number1 = gets.chomp
+
+  say "What is your second number?"
+  number2 = gets.chomp
+
+  say "Which calculation would you like to perform?"
+  puts "1) Add, 2) Subtract, 3) Multiply, 4) Divide"
+  calc_type  = gets.chomp
+
+  case calc_type
+    when "1"
+      answer = number1.to_i + number2.to_i
+    when "2"
+      answer = number1.to_i - number2.to_i
+    when "3"
+      answer = number1.to_i * number2.to_i
+    when "4"
+      answer = number1.to_f / number2.to_f
+  end
+
+  say "That equals #{answer}!"
+  say "Would you like to do another calculation? (Type 'yes' or 'no')"
+  again = gets.chomp
+end while again == "yes"
+
+
